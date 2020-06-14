b1569fe2fde0a833700cd2a9719b65c219e91e38
Marlena Ajoste <mpajoste@gmail.com>
2020-06-14 15:40:12 -0700

    created database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
+DROP DATABASE IF EXISTS ebid;
 
+CREATE DATABASE ebid;
+
+USE ebid;
+
+CREATE TABLE products (
+    id int NOT NULL AUTO_INCREMENT,
+    item VARCHAR(255) NOT NULL, 
+    min_cost FLOAT NOT NULL,
+    curr_bid FLOAT NOT NULL,
+    ends_in int NOT NULL,
+    image VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
